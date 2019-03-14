beac17449aad76a2dce55cbf7dd3bb226a04607e
vanichols <vnichols@iastate.edu>
2019-03-14 13:30:59 -0500

    FunTeam7 function, failed data add

---
+++ R/data.r
@@ -0,0 +1,20 @@
+#' Australia example
+#'
+#' A dataset of class sf that produces a map of Australia
+#'
+#' @source \url{https://gadm.org/download_country_v3.html}
+#' @docType data
+#' @keywords datasets
+#' @name ausex
+#' @format An sf object
+NULL
+
+#' Australia example out
+#'
+#' A tibble that is produced by the function FunTeam7 when applied to ausex
+#'
+#' @docType data
+#' @keywords datasets
+#' @name ausexout
+#' @format A tibble
+NULL
--- R/gina-T7-funs.R
+++ R/gina-T7-funs.R
@@ -1,41 +1,67 @@
-#' Thin shp file and gg-fy it
+#' Thin shp file and gg-ify it
 #'
-#' @param mydf dataframe of long/lats
-#' @return
-#' @export
-#'
+#' @param myfile dataframe of long/lats
+#' @param mytoler numeric, desired tolerance to thin, default is 0.1
+#' @return A tibble for ggplot2
+#' @examples
+#' # Take in sf data, use FunTeam7, plot output using ggplot2
+#' data(ausex)
+#' datatib <- FunTeam7(myfile = ausex)
+#' datatib %>%
+#'   ggplot(aes(x = long, y = lat, group = group)) +
+#'   geom_polygon(fill = "white", colour = "black", lwd = 1)+
+#'   theme_bw()+
+#'   coord_quickmap()
 #'
 
-FunLongLat <- function(mydf){
 
-  newdf <- data.frame(order = c(1:nrow(dframe)), long = dframe$x, lat = dframe$y)
-  return(newdf)
+FunTeam7 <- function(myfile, mytoler = 0.1) {
+  # If file is sf, continue w/analysis. Otherwise throw a message
+
+  if (is.numeric(mytoler) == TRUE) {
+
+    if (is(myfile, "sf") == TRUE) {
+      # If it passes, proceed to thin it
+
+      print(paste("Thinning to tolerance =", mytoler, "...be patient..."))
+
+      oz <-
+        maptools::thinnedSpatialPoly(
+          as(myfile, "Spatial"),
+          tolerance = mytoler,
+          minarea = 0.001,
+          topologyPreserve = TRUE
+        ) %>%
+        st_as_sf
+
+
+      FunLongLat <- function(dframe) {
+        newdf <-
+          data.frame(order = c(1:nrow(dframe)),
+                     long = dframe$x,
+                     lat = dframe$y)
+        return(newdf)
+      }
+
+      ozplus <-
+        oz$geometry %>%
+        modify_depth(3, data.frame) %>%
+        modify_depth(3, FunLongLat) %>%
+        flatten() %>%
+        flatten() %>%
+        bind_rows(.id = "group")
+
+      oztib <- as_tibble(ozplus)
+
+      print("Success! Use my output in ggplot2")
+      return(oztib)
+
+
+    } else {
+      print("Your file is not class sf. You broke me.")
+
+    }
+  } else {
+    print("Your tolerance is not numeric. You broke me.")
+  }
 }
-
-FunDoIt <- function(myfile, mytoler){
-
-  ozplus <- myfile$geometry
-
-
-
-
-}
-# ## Here our depth is 3, but that could change depending on the file
-# ozplus <- oz$geometry %>%
-#   modify_depth(3,data.frame) %>%
-#   modify_depth(3,f) %>%
-#   flatten() %>%
-#   flatten() %>%
-#   bind_rows(.id = "group")
-# # ozplus %>%
-# #   ggplot(aes(x = long, y = lat, group = group)) +
-# #   geom_polygon(fill = "white", colour = "black", lwd = 1)+
-# #   theme_bw()+
-# #   coord_quickmap()
-#
-#
-#
-# oz_st <- maptools::thinnedSpatialPoly(
-#   as(ozbig, "Spatial"), tolerance = 0.1,
-#   minarea = 0.001, topologyPreserve = TRUE)
-# oz <- st_as_sf(oz_st)
