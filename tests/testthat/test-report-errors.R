322945e95d1aba1a34e0f570c28d2586e92e8c98
qhe0917 <42719557+qhe0917@users.noreply.github.com>
2019-03-21 12:23:13 -0500

    Update test-report-errors.R

--- tests/testthat/test-report-errors.R
+++ tests/testthat/test-report-errors.R
@@ -1,7 +1,6 @@
 context("test-report-errors")
 
 test_that("errors are not reported properly", {
-  data(aus2rd)
-  expect_error(team_11(aus2rd, tol="A"),"tol is not numeric")
-  expect_error(team_11(aus2rd, tol=c(1:3)),"tol is not one number")
+  expect_error(team_11(system.file("shape/nc.shp", package="sf"), tol="A"),"tol is not numeric")
+  expect_error(team_11(system.file("shape/nc.shp", package="sf"), tol=c(1:3)),"tol is not one number")
 })
