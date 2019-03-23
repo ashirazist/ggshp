b2f9d44702d11382f7f46499302517ccbaac65c9
vanichols <vnichols@iastate.edu>
2019-03-23 17:44:25 -0500

    Fix team_7fun test error

--- tests/testthat/test-team_7test.R
+++ tests/testthat/test-team_7test.R
@@ -4,6 +4,6 @@ test_that("errors happen", {
 
   expect_error(team_7fun(system.file("shape/nc.shp", package="sf"), mytoler="A"),"Assertion on 'mytoler' failed: Must be of type 'number', not 'character'.")
 
-  expect_error(team_7fun(system.file("shape/nc.shp", package="sf"), mytoler =-7),"Assertion on 'mytoler' failed: All elements must be >= 0.")
+  expect_error(team_7fun(system.file("shape/nc.shp", package="sf"), mytoler =-7),"Assertion on 'mytoler' failed: Element 0 is not >= 0.")
 
 })
